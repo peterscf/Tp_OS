d3a85963900e8886ed83cba9467a6f0526cd94d4
Floriant PETERSCHMITT <floriant.peterschmitt@gmail.com>
2017-04-14 17:16:15 +0200

    .

--- ipc/player.c
+++ ipc/player.c
@@ -27,6 +27,7 @@ void detachSharedMemory()
        partagee, tous les processus l'ayant attache doivent le detacher. */
     
     /* TODO : Detacher le segment de memoire partagee avec shmdt() */
+	shmdt(sharedMemoryAddr);
 }
 
 
@@ -35,7 +36,9 @@ int main(int argc, char * argv[])
     key_t key;
     int queueId;
     int sharedMemoryId;
-
+#ifdef _SMART
+    int Stck=30;
+#endif
     msg_t message;
 
     action_t takeSticks;
@@ -54,25 +57,25 @@ int main(int argc, char * argv[])
     */
 
     /* TODO : ftok() */ 
-
+	key=ftok("./file_test.f",1);
 
     /* TODO : msgget()
        Le serveur ayant deja cree la file, pas besoin de flags ce coup-ci. 
     */
-
-
+	queueId = msgget(key,0);
     /* On dit bonjour au serveur en se presentant. */ 
     message.type = 1;
     sprintf(message.data, "%d", getpid());
 
     /* TODO : msgsnd -- envoyer le message */
-
+	msgsnd(queueId,&message,sizeof(message)-sizeof(long),0);
 
     /* On attend sa reponse. S'il n'y en a pas, c'est qu'il ne doit pas y
        avoir de serveur à l'autre bout de la file de message. Pour eviter
        d'attendre indefiniment une reponse, on enclenche une alarme. */
     alarm(3);
     /* TODO : msgrcv -- recevoir un message (type = getpid()) */
+	msgrcv(queueId,&message, sizeof(message)-sizeof(long) ,getpid(),0);
 
 
     /* Message recu, on desactive l'alarme. */
@@ -83,10 +86,13 @@ int main(int argc, char * argv[])
     sscanf(message.data, "%d", &sharedMemoryId);
 
     /* TODO : attacher le segment de memoire partagee à l'aide de shmat() */
+	 
+   	
+	sharedMemoryAddr= shmat(sharedMemoryId,NULL,0);
+   gameboard_t * gameboard = (gameboard_t *) sharedMemoryAddr;
+   	
 
-
-    gameboard_t * gameboard = (gameboard_t *) sharedMemoryAddr;
-    atexit(detachSharedMemory);
+	atexit(detachSharedMemory);
 
     printf("Connection to the game server: success.\n");
 
@@ -95,7 +101,8 @@ int main(int argc, char * argv[])
     {
         /* TODO : msgrcv -- recevoir un message (type =  getpid()) */
 
-        
+
+	msgrcv(queueId,&message, sizeof(message)-sizeof(long) ,getpid(),0);
         if(!strncmp("END", message.data, 3))
         {
             sscanf(message.data, "END %d", &winnerPid);
@@ -113,7 +120,7 @@ int main(int argc, char * argv[])
 
 #ifdef _ODD
         /* Il faut toujours un nombre impair de batonnets ! Toujours ! */
-        takeSticks = 1 + gameboard->nSticks % 2;
+        do{scanf("%d", &takeSticks);}while(takeSticks>3 || takeSticks < 1);
 #endif
 
 #ifdef _RANDOM
@@ -123,7 +130,16 @@ int main(int argc, char * argv[])
 
 #ifdef _SMART
         /* TODO : coder une IA avec un peu de jugeote. */
-        takeSticks = 1;
+		if(gameboard->nSticks == N_STICKS ){
+			Stck=N_STICKS;
+		}
+		if (Stck-gameboard->nSticks == 0){
+			takeSticks = 1;
+		}
+		else{
+			takeSticks = 4-(Stck-gameboard->nSticks);
+		}
+		Stck=gameboard->nSticks - takeSticks;
 #endif
 
         message.type = 1;
@@ -131,7 +147,8 @@ int main(int argc, char * argv[])
         printf("Taking %d of them.\n", takeSticks);
 
         /* TODO : msgsnd -- envoyer l'action à jouer */
-
+	
+	msgsnd(queueId,&message,sizeof(message)-sizeof(long),0);
     }
 
     return EXIT_SUCCESS;
